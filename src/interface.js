a2f33978307ddc2a14f1f9de14f4320031d30068
Vivian Allen <mrvivianallen@gmail.com>
2018-03-23 12:47:12 +0000

    city selection function working

--- src/interface.js
+++ src/interface.js
@@ -14,24 +14,31 @@ $( document ).ready(function() {
     thermostat.reset();
     updateTemperatureDisplay();
   })
-  $( "#get-weather" ).click(function() {
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1e2cd172300e71d547feea5fe13c32b0', function(data){
-      updateWeatherInfo(data);
-    })
+  $( "#get-weather" ).click(function(event) {
+    event.preventDefault();
+    var city = $( '#city-select' ).val();
+    var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q='
+              + city
+              + '&APPID=1e2cd172300e71d547feea5fe13c32b0';
+    console.log(apiCall);
+    $.get(apiCall, function(data){ updateWeatherInfo(data) });
   })
-
   $( "#PSMswitch" ).click(function() {
     thermostat._powersavingSwitch();
     updatePowersavingDisplay();
     updateTemperatureDisplay();
   })
+
   function updateTemperatureDisplay(){
-    $( "#temperature" ).text(thermostat.temperature() + ' °C');
+    $( "#temperature" ).text(thermostat.temperature() + '°C');
   }
   function updatePowersavingDisplay(){
     $( "#PSM" ).text('Powersaving: ' + thermostat.powersaving());
   }
   function updateWeatherInfo(data){
-    $( "#weather-info" ).text("Weather: " + data.weather[0].description + ", Temperature: " + Math.round(data.main.temp_max - 273.15));
+    $( "#weather-info" ).text(data.weather[0].description
+                              + ", "
+                              + Math.round(data.main.temp_max - 273.15)
+                              + "°C");
   }
 });
